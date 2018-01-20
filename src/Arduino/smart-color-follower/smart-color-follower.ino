ec7b4196b534635a44d042ad0901d452b0cd3146
= <berickson@gmail.com>
2018-01-19 16:49:19 -0800

    Goes fixed distances with bluetooth command.

--- src/Arduino/smart-color-follower/smart-color-follower.ino
+++ src/Arduino/smart-color-follower/smart-color-follower.ino
@@ -13,15 +13,154 @@ const int pin_left_rev = 6;
 const int pin_right_fwd = 21;
 const int pin_right_rev = 22;
 
+const int pin_right_a = 17;
+const int pin_right_b = 16;
+
 const int pin_left_a = 15;
 const int pin_left_b = 4;
 const int pin_battery_voltage_divider = A9;
+
+#define Bluetooth Serial3
 
 
 int sign_of(int i) {
   if (i<0) return -1;
   return 1;
 }
+#define __GXX_RTTI
+#include <functional>
+
+class QuadratureEncoder {
+private:
+  std::function<void()> callback_a;
+  std::function<void()> callback_b;
+public:
+  const int pin_a;
+  const int pin_b;
+
+  long a_ticks = 0;
+  long b_ticks = 0;
+  unsigned int a_us;
+  unsigned int b_us;
+  
+
+  QuadratureEncoder(int pin_a, int pin_b) : pin_a(pin_a), pin_b(pin_b){
+    callback_a = std::bind( &QuadratureEncoder::on_a_changed, *this );
+    
+    callback_b = std::bind( &QuadratureEncoder::on_b_changed, *this );
+    //attachInterrupt(pin_b, callback_b, CHANGE);
+  }
+
+  inline void on_a_changed() {
+    a_us = micros();
+    if(digitalReadFast(pin_a)==digitalReadFast(pin_b)) {
+      --a_ticks;
+    } else {
+      ++a_ticks;
+    }
+  }
+
+  inline void on_b_changed() {
+    b_us = micros();
+    if(digitalReadFast(pin_a)==digitalReadFast(pin_b)) {
+      ++b_ticks;
+    } else {
+      --b_ticks;
+    }
+  }
+
+  inline long get_ticks() {
+    return a_ticks + b_ticks;
+    
+  }
+};
+
+class Speedometer {
+public:
+  QuadratureEncoder & encoder;
+  float meters_per_tick = 0; // set by client
+  int last_odo_a = 0;
+  int last_odo_b = 0;
+  unsigned int last_a_us = 0;
+  unsigned int last_b_us = 0;
+  unsigned int last_ab_us = 0;
+  unsigned int last_clock_us = 0;
+  volatile float velocity = 0;
+  volatile float acceleration = 0;
+  volatile float v_a;
+  volatile float v_b;
+  
+  Speedometer(QuadratureEncoder & encoder, float meters_per_tick) : encoder(encoder), meters_per_tick(meters_per_tick) {}
+  
+  void update() {
+    float last_v = velocity;
+
+    auto a_us = encoder.a_us;
+    auto b_us = encoder.b_us;
+    auto odo_a = encoder.a_ticks;
+    auto odo_b = encoder.a_ticks;
+    auto clock_us = micros();
+    
+    if (a_us > last_a_us) {
+      v_a =  (odo_a-last_odo_a)*2*meters_per_tick / (a_us - last_a_us) *1E6;
+    } 
+    if(b_us != last_b_us) {
+      v_b =  (odo_b-last_odo_b)*2*meters_per_tick / (b_us - last_b_us) *1E6;
+    }
+    velocity = (v_a + v_b) / 2.;
+  
+  
+    unsigned tick_us = std::max(a_us,b_us);
+    unsigned last_tick_us = std::max(last_a_us, last_b_us);
+    float elapsed_seconds = (tick_us - last_tick_us) / 1000000.;
+    if (elapsed_seconds == 0) {
+      // no tick this time, how long has it been?
+      elapsed_seconds= ( clock_us - last_tick_us) / 1000000.;
+      if (elapsed_seconds > 0.1){
+        // it's been a long time, let's call velocity zero
+        velocity = 0.0;
+      } else {
+        // we've had a tick recently, fastest possible speed is when a tick is about to happen
+        // do nothing unless smaller than previously certain velocity
+        float  max_possible = meters_per_tick / elapsed_seconds;
+        if(max_possible < fabs(velocity)){
+          if(velocity > 0)
+            velocity = max_possible;
+          else
+            velocity = -max_possible;
+        }
+      }
+    }
+    if(elapsed_seconds > 0) {
+      acceleration = (velocity - last_v) / elapsed_seconds;
+    }
+  
+    last_odo_a = odo_a;
+    last_odo_b = odo_b;
+    last_a_us  = a_us;
+    last_b_us  = b_us;
+    last_clock_us = clock_us;
+  }
+
+  float get_odo_meters() {
+    return (last_odo_a + last_odo_b) * meters_per_tick;
+  }
+};
+
+
+QuadratureEncoder right_encoder(pin_right_a, pin_right_b);
+Speedometer speedometer(right_encoder, 1./4225);
+
+// unfortunatly, we need external callbacks
+void on_right_a_changed () {
+  right_encoder.on_a_changed();
+}
+
+void on_right_b_changed () {
+  right_encoder.on_b_changed();
+}
+
+
 
 class Motor {
   public:
@@ -39,6 +178,8 @@ class Motor {
       pinMode(pin_rev, OUTPUT);
       analogWriteFrequency(pin_fwd, pwm_frequency);
       analogWriteFrequency(pin_rev, pwm_frequency);
+      attachInterrupt(right_encoder.pin_a, on_right_a_changed, CHANGE);
+      attachInterrupt(right_encoder.pin_b, on_right_b_changed, CHANGE);
     }
 
     void set_speed_raw(int speed) {
@@ -53,6 +194,7 @@ class Motor {
     }
 
     void set_speed_percent(int percent) {
+      percent = constrain(percent, -100, 100);
       int power = abs(percent);
       int raw_power = ( percent == 0) ? 0 : map(power, 0, 100, power_stall, power_max);
       
@@ -63,11 +205,6 @@ class Motor {
 
 Motor motor_left(pin_left_fwd, pin_left_rev);
 Motor motor_right(pin_right_fwd, pin_right_rev);
-
-
-
-
-
 
 
 #include <Adafruit_GFX.h>
@@ -89,6 +226,8 @@ Motor motor_right(pin_right_fwd, pin_right_rev);
 
 const int display_width = 96;
 const int display_height = 64;
+
+int speed_percent_sp = 50;
 
 // button that is tied directly to two digital pins
 // the only reason you would do this is if it is easier
@@ -133,6 +272,15 @@ public:
   }
 
   void setup() {
+    Serial.begin(115200);
+    while(!Serial){}
+    Bluetooth.begin(9600);
+    Serial.print("waiting for Bluetooth...");
+    while(!Bluetooth){
+      delay(1);
+    }
+    Serial.println("ready");
+    
     pinMode(pin, INPUT);
   }
 
@@ -160,10 +308,88 @@ const int t_delay=0;
 
 
 // buffer must be big enough for command, arguments, terminiators and null
-char serial_command_buffer_[100];
-SerialCommands serial_commands(&Serial1, serial_command_buffer_, sizeof(serial_command_buffer_), (char *)"\r\n", (char *)" ");
+char camera_command_buffer_[100];
+SerialCommands camera_commands(&Serial1, camera_command_buffer_, sizeof(camera_command_buffer_), (char *)"\r\n", (char *)" ");
+char bluetooth_command_buffer_[100];
+SerialCommands bluetooth_commands(&Bluetooth, bluetooth_command_buffer_, sizeof(bluetooth_command_buffer_), (char *)"\r\n", (char *)" ");
 
 
+void cmd_set_speed(SerialCommands * sender) {
+  speed_percent_sp = atoi(sender->Next());
+}
+
+void cmd_left(SerialCommands * sender) {
+  motor_left.set_speed_percent(-speed_percent_sp);
+  motor_right.set_speed_percent(speed_percent_sp);
+}
+
+void cmd_right(SerialCommands * sender) {
+  motor_left.set_speed_percent(speed_percent_sp);
+  motor_right.set_speed_percent(-speed_percent_sp);
+}
+
+void cmd_go(SerialCommands * sender) {
+  sender->GetSerial()->println("inside go");
+  float d = atof(sender->Next());
+  float destination_meters = d + speedometer.get_odo_meters();
+  float velocity_max = atof(sender->Next());
+  float throttle = 0;
+  float k_p = 2.0;
+  float k_d = 100;
+  float last_error = 0;
+  float last_position_error = 0;
+
+  float position_k_p = 10;
+  float position_k_d = 200;
+  
+  motor_left.set_speed_percent(throttle);
+  motor_right.set_speed_percent(throttle);
+
+  sender->GetSerial()->println((String) "go " + d + " " + velocity_max);
+  
+
+  while(speedometer.get_odo_meters() < destination_meters) {
+    delay(1);
+    speedometer.update();
+    loop();
+
+    float position_error = destination_meters - speedometer.get_odo_meters();
+    float velocity_sp = position_k_p * position_error + position_k_d * (position_error-last_position_error);
+    velocity_sp = constrain(velocity_sp, -velocity_max, velocity_max);
+    sender->GetSerial()->println((String)position_error + " v_sp: " + velocity_sp);
+
+    float error = velocity_sp - speedometer.velocity;
+    float d_error = error-last_error;
+    throttle += k_p * error + k_d * d_error;
+    motor_left.set_speed_percent(throttle);
+    motor_right.set_speed_percent(throttle);
+    last_error = error;
+    last_position_error = position_error;
+  }
+  
+  motor_left.set_speed_percent(0);
+  motor_right.set_speed_percent(0);
+  delay(1000);
+  speedometer.update();
+  sender->GetSerial()->println((String)"final distance: " + (destination_meters - speedometer.get_odo_meters()));
+}
+
+
+
+void cmd_fwd(SerialCommands * sender) {
+  motor_left.set_speed_percent(speed_percent_sp);
+  motor_right.set_speed_percent(speed_percent_sp);
+}
+
+void cmd_rev(SerialCommands * sender) {
+  motor_left.set_speed_percent(-speed_percent_sp);
+  motor_right.set_speed_percent(-speed_percent_sp);
+}
+
+void cmd_stop(SerialCommands * sender) {
+  motor_left.set_speed_percent(0);
+  motor_right.set_speed_percent(0);
+}
 
 void on_colors_sensed(SerialCommands * sender) {
   char * bottom = sender->Next();
@@ -245,14 +471,31 @@ void show_goalpost(const char * bottom, const char * left, const char * right) {
 
 SerialCommand cmd_colors_sensed("cld",on_colors_sensed);
 
+SerialCommand cmd_go_sensed("go",cmd_go);
+SerialCommand cmd_fwd_sensed("fwd",cmd_fwd);
+SerialCommand cmd_rev_sensed("rev",cmd_rev);
+SerialCommand cmd_left_sensed("left",cmd_left);
+SerialCommand cmd_right_sensed("right",cmd_right);
+SerialCommand cmd_stop_sensed("stop",cmd_stop);
+SerialCommand cmd_set_speed_sensed("speed",cmd_set_speed);
+
 void setup(void) {
   Serial1.begin(115200);
   display.begin();
   motor_left.setup();
   motor_right.setup();
   display.fillScreen(GRAY);
-  serial_commands.SetDefaultHandler(on_unrecognized);
-  serial_commands.AddCommand(&cmd_colors_sensed);
+  camera_commands.SetDefaultHandler(on_unrecognized);
+  camera_commands.AddCommand(&cmd_colors_sensed);
+
+  Bluetooth.begin(9600);
+  bluetooth_commands.AddCommand(&cmd_go_sensed);
+  bluetooth_commands.AddCommand(&cmd_fwd_sensed);
+  bluetooth_commands.AddCommand(&cmd_rev_sensed);
+  bluetooth_commands.AddCommand(&cmd_left_sensed);
+  bluetooth_commands.AddCommand(&cmd_right_sensed);
+  bluetooth_commands.AddCommand(&cmd_stop_sensed);
+  bluetooth_commands.AddCommand(&cmd_set_speed_sensed);
   
 }
 
@@ -304,10 +547,15 @@ void loop() {
   const int run_mode_count = 12;
   
   loop_checker.run();
-  serial_commands.ReadSerial();
-  bool boundary_1_s = loop_checker.every_n_ms(1000);
-
+  camera_commands.ReadSerial();
+  bluetooth_commands.ReadSerial();
+  //bool boundary_1_s = loop_checker.every_n_ms(1000);
+  bool boundary_10_ms = loop_checker.every_n_ms(10);
   bool boundary_100_ms = loop_checker.every_n_ms(100);
+
+  if(boundary_10_ms) {
+    speedometer.update();
+  }
 
   if(boundary_100_ms) {
     if(button1.was_clicked()) {
@@ -392,16 +640,16 @@ void loop() {
     drawTile(50,0);
   }
 */
-  if(boundary_1_s) {
+  if(boundary_100_ms) {
     //display.fillScreen(GRAY);
     display.setCursor(10,10);
-    String v_string = (String)v_bat.get_voltage()+"v";
+    String v_string = (String)v_bat.get_voltage()+"v " + speedometer.velocity + " "; // space helps drawing issues
     int16_t x1,y1;
     uint16_t h, w;
     display.getTextBounds((char *)v_string.c_str(), 10, 10,
       &x1, &y1, &w, &h);
     display.fillRect(x1,y1,w,h,GRAY);
-    display.print(String(v_bat.get_voltage())+"v");
+    display.print(v_string);
     Serial.println((String)"Loop count: " + loop_checker.loop_count);
   }
 }
